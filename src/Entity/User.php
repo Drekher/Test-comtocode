0dc1a60849dea3a5063201186114e254c4e005c2
Drekher <bertrand.fernandez@gmail.com>
2019-02-25 14:33:21 +0100

    Registration / Login / Logout with FosUser Bundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -29,6 +29,55 @@ class User extends BaseUser
     private $firstname;
     
     
+    
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
     public function __construct()
     {}
 }
